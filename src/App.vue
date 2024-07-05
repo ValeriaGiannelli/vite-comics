e339d2190e0742e73e0d0b36056d0bc0f438638f
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-07-05 14:44:00 +0200

    feat: create header, insert it on the App.vue and style

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
+
+export default{
+  name: "App",
+  components: {
+    Header,
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Header/>
 </template>
 
-<style>
+<style lang="scss">
+  @use './styles/general.scss';
+
+  
 
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,78 @@
+<script>
+    export default{
+        name: "Header",
+    }
+
+</script>
+
+<template>
+    <div class="container_menu">
+        <!-- logo -->
+        <div class="logo">
+            <img src="../assets/img/dc-logo.png" alt="Logo DC">
+        </div>
+
+        <!-- menu navigazione -->
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">CHARACTERS</a>
+                </li>
+                <li>
+                    <a href="#">CHARACTERS</a>
+                </li>
+                <li>
+                    <a href="#">CHARACTERS</a>
+                </li>
+                <li>
+                    <a href="#">CHARACTERS</a>
+                </li>
+                <li>
+                    <a href="#">CHARACTERS</a>
+                </li>
+                <li>
+                    <a href="#">CHARACTERS</a>
+                </li>
+                <li>
+                    <a href="#">CHARACTERS</a>
+                </li>
+                <li>
+                    <a href="#">CHARACTERS</a>
+                </li>
+                <li>
+                    <a href="#">CHARACTERS</a>
+                </li>
+                <li>
+                    <a href="#">CHARACTERS</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+
+    
+
+</template>
+
+<style lang="scss" scoped>
+.container_menu{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 80%;
+    margin: 0 auto;
+    padding: 20px;
+
+    .logo{
+        max-width: 80px;
+    }
+
+    ul{
+        display: flex;
+        gap: 10px;
+        font-size: 12px;
+        list-style-type: none;
+    }
+}
+
+
+</style>
